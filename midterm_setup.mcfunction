655f8fa39fc9d08e69f9e67616acf103a8b80c62
Zhengze Gong <43304098+harry8698@users.noreply.github.com>
2020-02-25 17:47:01 -0500

    Add files via upload

---
+++ midterm_setup.mcfunction
@@ -0,0 +1,11 @@
+clear @p[distance=..2]
+tp @p[distance=..2,name=appleseedorchard] 63 110 59
+tp @p[distance=..2,name=WettestFrog] 63 110 187
+tp @p[distance=..2,name=izofar] 63 110 315
+tp @p[distance=..2,name=rx78v] 63 110 443
+tp @p[distance=..2,name=DripOrDrown99] 63 110 571
+tp @p[distance=..2,name=papaseb] 191 110 59
+tp @p[distance=..2,name=russellwalker21] 191 110 187
+tp @p[distance=..2,name=Tianfei] 191 110 315
+tp @p[distance=..2,name=Armitheo] 191 110 443
+tp @p[distance=..2,name=DouyuTV20120701] 191 110 571
---
+++ midterm_setup.py
@@ -0,0 +1,36 @@
+class Coordinates:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Rooms:
+    rowDistance = 128
+    colDistance = 128
+    anchor = Coordinates(63, 110, 59)
+
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col 
+        self.rooms = [[""] * col for i in range(row)]
+
+    def assign(self, names):
+        assert (len(names) < self.col * self.row)
+        f = open ('./midterm_setup.mcfunction', "w+")
+        f.write ("clear @p[distance=..2]\n")
+        i, j = 0, 0
+        for name in names:
+            self.rooms[i][j] = name
+            f.write ("tp @p[distance=..2,name={}] {} {} {}\n".format(name, 
+                                                            self.anchor.x + self.rowDistance * i,
+                                                            self.anchor.y,
+                                                            self.anchor.z + self.colDistance * j))
+            i = i + 1 if j + 1 == self.col else i
+            j = (j + 1) % self.col
+        f.close()
+
+
+nameList = ['appleseedorchard', 'WettestFrog', 'izofar', 'rx78v', 'DripOrDrown99', 
+            'papaseb', 'russellwalker21', 'Tianfei', 'Armitheo', 'DouyuTV20120701']
+rooms = Rooms (6, 5)
+rooms.assign (nameList)
