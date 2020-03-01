bb24a90f1b5a847d1925caf93ccaa6a728462d5d
Zhengze Gong <zhengzeg@andrew.cmu.edu>
2020-02-29 22:38:47 -0500

    added Player class that stores each player's name and coordinates, added toString methods for printing useful messages, can now accept cmd line arguments to get a specific player's loaction

--- midterm_setup.py
+++ midterm_setup.py
@@ -1,8 +1,27 @@
+import sys
+
 class Coordinates:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+
+class Player:
+    def __init__(self, name, coordinates):
+        self.name = name
+        self.coordinates = coordinates
+    
+    def getX(self):
+        return self.coordinates.x
+    
+    def getY(self):
+        return self.coordinates.y
+
+    def getZ(self):
+        return self.coordinates.z
+    
+    def toString(self):
+        return "{} : {} {} {}".format(self.name, self.getX(), self.getY(), self.getZ())
 
 class Rooms:
     rowDistance = 128
@@ -12,37 +31,44 @@ class Rooms:
     def __init__(self, row, col):
         self.row = row
         self.col = col 
-        self.rooms = [[""] * col for i in range(row)]
+        self.players = {}
 
     def assign(self, names):
         assert (len(names) < self.col * self.row)
         f = open ('./midterm_setup.mcfunction', "w+")
         f.write ("clear @p[distance=..2]\n")
         i, j = 0, 0
+        self.names = names
         for name in names:
-            self.rooms[i][j] = name
-            f.write ("tp @p[distance=..1,name={}] {} {} {}\n".format(name, 
-                                                            self.anchor.x + self.rowDistance * i,
-                                                            self.anchor.y,
-                                                            self.anchor.z + self.colDistance * j))
+            coordinates = Coordinates (self.anchor.x + self.rowDistance * i, 
+                                       self.anchor.y,
+                                       self.anchor.z + self.colDistance * j)
+            player = Player (name, coordinates)
+            self.players[name] = player
+            f.write ("tp @p[distance=..1,name={}] {} {} {}\n".format(player.name, 
+                                                                     player.getX(),
+                                                                     player.getY(),
+                                                                     player.getZ()))
             i = i + 1 if j + 1 == self.col else i
             j = (j + 1) % self.col
         f.close()
 
-    def get(self, name, names):
-        assert (len(names) < self.col * self.row)
-        i, j = 0, 0
-        for n in names:
-            if n == name:
-                print ("The coordinates for {} is {} {} {}".format(name, 
-                                                            self.anchor.x + self.rowDistance * i,
-                                                            self.anchor.y,
-                                                            self.anchor.z + self.colDistance * j))
-                return
-            i = i + 1 if j + 1 == self.col else i
-            j = (j + 1) % self.col
-        print ("Player not found")
+    def get(self, name):
+        if (name in self.players):
+            return self.players[name].toString()
+        return "Player not found"
+
+    def toString(self):
+        result = ""
+        for p in self.players.values():
+            result += p.toString() + "\n"
+        return result
+        
 
 nameList = ['appleseedorchard', 'WettestFrog', 'izofar', 'rx78v', 'DripOrDrown99', 'papaseb', 'russellwalker21', 'Tianfei', 'Armitheo', 'DouyuTV20120701', 'RX78v', 'Omegatron', 'The1stDarkTower', 'Flightraid', 'Youcef_Tlm', 'Deter_Mined', 'flamegator', 'LaKoixFish', 'Dummyc0m', 'rf174919', 'Trollium', 'raphy2', 'stmjk', 'Cloudberryz', 'fredjoon']
 rooms = Rooms (6, 5)
 rooms.assign (nameList)
+print (rooms.toString())
+
+if (len(sys.argv) == 2):
+    print(rooms.get(sys.argv[1], nameList))
