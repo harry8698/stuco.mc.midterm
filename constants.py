60d3b71a156485bf894debb9b63fb13888bcb805
Zhengze Gong <zhengzeg@andrew.cmu.edu>
2020-03-11 01:16:36 -0400

    added colors constant to git

--- constants.py
+++ constants.py
@@ -5,3 +5,5 @@ COL_DISTANCE = 128
 ANCHOR_X = 63
 ANCHOR_Y = 110
 ANCHOR_Z = 59
+COLORS = ['brown', 'magenta', 'pink', 'purple', 'cyan', 'blue', 'light_blue', 
+          'lime', 'green', 'yellow', 'orange', 'red']
